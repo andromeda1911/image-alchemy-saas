ba98ae4acfd68b84994bf7a2e2b920951f92ccfb
Nikhil <gemnik94@gmail.com>
2024-02-27 01:16:32 +0530

    changed upload preset

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="imagealchemy"
       options={{
         multiple: false,
         resourceType: "image",
