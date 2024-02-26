3eb33199ce2ca0e867423a49f3f9d1e606412175
Nikhil <gemnik94@gmail.com>
2024-02-26 16:43:21 +0530

    changed mongoDB url

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,32 +1,31 @@
-import mongoose, { Mongoose } from 'mongoose';
+import mongoose, {Mongoose} from 'mongoose'
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URL = process.env.MONGODB_URL
 
 interface MongooseConnection {
-  conn: Mongoose | null;
-  promise: Promise<Mongoose> | null;
+    conn: Mongoose | null;
+    promise: Promise<Mongoose> | null;
 }
 
 let cached: MongooseConnection = (global as any).mongoose
 
 if(!cached) {
-  cached = (global as any).mongoose = { 
-    conn: null, promise: null 
-  }
+    cached = (global as any).mongoose = {
+        conn: null, promise: null
+    }
 }
 
-export const connectToDatabase = async () => {
-  if(cached.conn) return cached.conn;
+export const connectToDb = async () => {
+    console.log('trying to establish connection to db')
+    if(cached.conn) return cached.conn;
 
-  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+    if(!MONGODB_URL) throw new Error('MONGO DB url is mising');
 
-  cached.promise = 
-    cached.promise || 
-    mongoose.connect(MONGODB_URL, { 
-      dbName: 'imaginify', bufferCommands: false 
-    })
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, { 
+        dbName: 'imagealchemy', bufferCommands: false 
+      })
 
-  cached.conn = await cached.promise;
-
-  return cached.conn;
+    cached.conn = await cached.promise;
+    console.log('conn value', cached.conn);
+    return cached.conn;
 }
